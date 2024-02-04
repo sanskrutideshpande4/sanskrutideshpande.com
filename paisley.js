a284d6a05cc1dbc6597fe9513e53e00f71fba250
sanskrutideshpande4 <deshpandesanskruti4@gmail.com>
2024-02-05 03:40:49 +0530

    second commit 5 Feb 2024

---
+++ paisley.js
@@ -0,0 +1,25 @@
+
+
+$(document).ready(function () {
+
+    $('#nav-about').on('click' , function() {
+        navigate('#about');
+    });
+
+
+    $('#nav-moodboard').on('click' , function() {
+        navigate('#moodboard');
+    });
+
+    $('#nav-exploringpatterns').on('click' , function() {
+        navigate('#exploringpatterns');
+    });
+
+    $('#nav-applications').on('click' , function() {
+        navigate('#applications');
+    });
+
+
+
+});
+
