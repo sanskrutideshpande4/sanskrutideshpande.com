d3d6170f1d42a75372cc21ccb52e32733320e7ec
Samruddhi <sadeshpande24@gmail.com>
2024-11-04 00:31:57 -0500

    10th commit - 4 Nov 2024

---
+++ lamp.js
@@ -0,0 +1,39 @@
+
+
+$(document).ready(function () {
+
+    $('#nav-about').on('click' , function() {
+        navigate('#about');
+    });
+    
+    $('#nav-ideation').on('click' , function() {
+        navigate('#ideation');
+    });
+
+    $('#nav-moodboard').on('click' , function() {
+        navigate('#moodboard');
+    });
+
+    $('#nav-sketches').on('click' , function() {
+        navigate('#sketches');
+    });
+
+    $('#nav-processlibrary').on('click' , function() {
+        navigate('#processlibrary');
+    });
+
+    $('#nav-finalpictures').on('click' , function() {
+        navigate('#finalpictures');
+    });
+
+    $('#nav-photolibrary').on('click' , function() {
+        navigate('#photolibrary');
+    });
+
+    $('#nav-lyricsplaybyplay').on('click' , function() {
+        navigate('#lyricsplaybyplay');
+    });
+
+
+
+});
