2d9f25df1db02417fb70b734c508570f733d0be9
Samruddhi <sadeshpande24@gmail.com>
2024-11-26 12:00:07 -0500

    16th commit - 26 Nov 2024

---
+++ lamp.js
@@ -0,0 +1,41 @@
+
+
+$(document).ready(function () {
+
+    $('#nav-1').on('click' , function() {
+        navigate('#1');
+    });
+    
+    $('#nav-2').on('click' , function() {
+        navigate('#2');
+    });
+
+    $('#nav-3').on('click' , function() {
+        navigate('#3');
+    });
+
+    $('#nav-4').on('click' , function() {
+        navigate('#4');
+    });
+
+    $('#nav-5').on('click' , function() {
+        navigate('#5');
+    });
+
+    $('#nav-6').on('click' , function() {
+        navigate('#6');
+    });
+
+    $('#nav-7').on('click' , function() {
+        navigate('#7');
+    });
+
+    $('#nav-8').on('click' , function() {
+        navigate('#8');
+    });
+    
+    $('#nav-9').on('click' , function() {
+        navigate('#9');
+    });
+    
+});
---
+++ penstand.js
@@ -0,0 +1,55 @@
+
+
+$(document).ready(function () {
+
+    $('#nav-cover').on('click' , function() {
+        navigate('#cover');
+    });
+    
+    $('#nav-problem').on('click' , function() {
+        navigate('#problem');
+    });
+
+    $('#nav-solution').on('click' , function() {
+        navigate('#solution');
+    });
+
+    $('#nav-userflow').on('click' , function() {
+        navigate('#userflow');
+    });
+
+    $('#nav-wireframes').on('click' , function() {
+        navigate('#wireframes');
+    });
+
+    $('#nav-testing').on('click' , function() {
+        navigate('#testing');
+    });
+
+    $('#nav-insights').on('click' , function() {
+        navigate('#insights');
+    });
+
+    $('#nav-features').on('click' , function() {
+        navigate('#features');
+    });
+    
+    $('#nav-designguidelines').on('click' , function() {
+        navigate('#designguidelines');
+    });
+    
+    $('#nav-styleguide').on('click' , function() {
+        navigate('#styleguide');
+    });
+    
+    $('#nav-smartphonemockups').on('click' , function() {
+        navigate('#smartphonemockups');
+    });
+    
+    $('#nav-devicemockups').on('click' , function() {
+        navigate('#devicemockups');
+    });
+
+
+
+});
